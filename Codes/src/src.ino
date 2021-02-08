293c948c93cfb91d3688b46504c2aeba431c2acd
Aditya Kumar Singh <adityasingh3007@gmail.com>
2021-02-08 22:08:37 +0530

    Spider text/logo removed. AP mode removed

--- Codes/src/src.ino
+++ Codes/src/src.ino
@@ -1,6 +1,6 @@
 /*
 **************************************************************************************
-                       Avrdude for ESP32
+                                SmartBin
                    ================================
    Version: 0.7.0
 ***************************************************************************************
@@ -22,23 +22,11 @@
                 USER SHOULD CHANGE THIS PART OF THE CODE ONLY
 */
 
-/*  Select the WiFi Mode from Station Mode and Access Point(AP) Mode.
- *  In AP Mode, the nod will create its own WiFi hotspot.
- *  In Station mode, node will get connected to an existing WiFi
- *  0 -> AP Mode, 1 -> Station Mode
- *  Default is AP Mode.
-*/
-#define OTA_WIFI_MODE 1
-
 /*
-   If Wifi mode is selected as Station i.e 1, ESP32 will connect to
-   one of the exisitng Wifi Network. Hence, provide the existing Wifi
-   network credentials.
+   Provide the existing Wifi network credentials.
 */
-#if (OTA_WIFI_MODE == 1)
-  const char* ssid = "MaataRaani";     //NETWORK_SSID_HERE
-  const char* password = "698630LAK";  //NETWORK_PASSWORD_HERE
-#endif
+const char* ssid = "NETWORK_SSID_HERE";
+const char* password = "NETWORK_PASSWORD_HERE";
 
 /*
  * Microcotroller and its Clock Setting
@@ -56,24 +44,15 @@
                       USER CHANGEABLE AREA ENDED
    ==============================================================
 */
-/*
-   If Wifi mode is selected as AP i.e 0, ESP32 will create its
-   own Wifi Network. The credentials will be-
-   SSID = Avrdude
-   Password = spider.123
-*/
-#if (OTA_WIFI_MODE == 0)
-  const char* ssid = "Avrdude";
-  const char* password = "spider.123";
-#endif
 
-const char* host = "avrdude";                                    // Host name for mDNS server
+const char* host = "smartbin";                                      // Host name for mDNS server
 
-WebServer server(80);                                            // Creating WebServer object on port 80
+WebServer server(3000);                                            // Creating WebServer object on port 80
+
 WiFiClient client;
 
 /* Global declarations for OTA Logic */
-String resp = "Some error occured. Please re-try";               // Response from avrdude will be stored here 
+String resp = "Some error occured. Please re-try";                // Response from avrdude will be stored here 
 uint8_t file[32768] PROGMEM;
 uint8_t prog[32768] PROGMEM;
 int file_index = 0;
@@ -122,31 +101,16 @@ void setup(void) {
       Serial.begin(1200);
     #endif
   #endif 
+  Serial.println(""); 
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
   Serial.println("");
-  #ifdef OTA_WIFI_MODE
-    #if (OTA_WIFI_MODE == 0)
-      Serial.println("Setting AP (Access Point)..");
-      WiFi.softAP(ssid, password);
-      IPAddress IP = WiFi.softAPIP();
-      Serial.print("Network SSID: ");Serial.println(ssid);
-      Serial.print("Network Password: ");Serial.println(password);
-      Serial.print("AP IP address: "); Serial.println(IP);
-    #elif (OTA_WIFI_MODE == 1)
-      Serial.print("Connecting to WiFi");
-      WiFi.begin(ssid, password);
-      while (WiFi.status() != WL_CONNECTED) {
-        delay(250);
-        Serial.print(".");
-      }
-      Serial.println("");
-      Serial.print("Connected to "); Serial.println(ssid);
-      Serial.print("IP address: "); Serial.println(WiFi.localIP());
-    #else
-      #error Un-identfied WiFi Mode
-    #endif
-  #else
-    #error WiFi mode not defined
-  #endif
+  Serial.print("Connected to "); Serial.println(ssid);
+  Serial.print("IP address: "); Serial.println(WiFi.localIP());
   /* Using mdns for host name resolution */
   if (!MDNS.begin(host)) {                                 
     Serial.println("Error setting up MDNS responder!");
@@ -154,7 +118,7 @@ void setup(void) {
       delay(1000);
     }
   }
-  Serial.print("mDNS responder started.\nServing at http://"); Serial.print(host); Serial.println(".local      on Port 80");
+  Serial.print("mDNS responder started.\nServing at http://"); Serial.print(host); Serial.println(".local      on Port 3000");
   
   // Route for root / web page
   server.on("/", HTTP_GET, []() {
